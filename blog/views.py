9be755ed7986196bcb115050c5655bb4aaf1cf78
VidalYT <vidalgames12345@gmail.com.com>
2026-04-16 18:57:21 -0700

    Was added teh section forms in the page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404, render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Post, Category
+from .forms import CommentForm
 
 # The code was ordened for a minor complex code.
 def home(request):
@@ -14,7 +15,20 @@ def home(request):
 def detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.ACTIVE)
 
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', id=id)
+    else:
+        form = CommentForm()
+
     context = {
         'post': post,
+        'form': form
     }
     return render(request, 'blog/detail.html', context)
